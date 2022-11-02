db6a25cc6c443f78aa3a1b299f944e80647129b2
Jesse Zhang <115027318+JesseZhang1031@users.noreply.github.com>
2022-11-02 16:44:30 -0700

    Update website.js

--- JS/website.js
+++ JS/website.js
@@ -11,4 +11,8 @@ function validate()
     if( document.getElementById("emailAddr").value.length == 0 ) {
      alert( "Please enter your Email!" );
     }
+
+    if( (document.getElementById("emailAddr").value.length >= 1 ) & ( document.getElementById("lname").value.length >= 1 ) & ( document.getElementById("emailAddr").value.length >= 1 )){
+        alert( "Thank you! I will contact you back as soon as possible." );
+       }
 }
